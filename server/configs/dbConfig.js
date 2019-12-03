3db96f49611a8e2ce70e41ef017e9f48334d8d3a
LUA ZHI ZHAN <19B758T@sit.nyp.edu.sg>
2019-12-03 14:17:33 +0800

    update package json and ci file

--- server/configs/dbConfig.js
+++ server/configs/dbConfig.js
@@ -1,9 +1,9 @@
 const dbConfig = {
   connectionLimit: 100,
-  host: "182.50.133.88",
-  user: "itj153user",
-  password: "itj153user",
-  database: "herokuSample"
+  host: '182.50.133.88',
+  user: 'itj153user',
+  password: 'itj153user',
+  database: 'herokuSample'
 };
 
 module.exports = dbConfig;
