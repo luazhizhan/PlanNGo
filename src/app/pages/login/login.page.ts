6660032e61119b223848c51ee8cb0db91c78a4eb
Lua Zhi Zhan <zhizhanlua@gmail.com>
2019-12-07 11:34:46 +0800

    update login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+import { AuthService } from '../../services/auth.service'
 
 @Component({
   selector: 'app-login',
@@ -6,7 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-  constructor() {}
+  authForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private navCtrl: NavController, private authSvc: AuthService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.authForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      pwd: ['', [Validators.required]],
+    }, {});
+  }
+
+  loginAuth(values) {
+    if (this.authForm.invalid) {
+      return alert('Please enter your username and password');
+    }
+    this.authSvc.login(values.username, values.pwd).subscribe(validUser => {
+      if (validUser) {
+        this.navCtrl.navigateForward('/tabs/home');
+      } else {
+        return alert('Wrong username or password');
+      }
+    })
+  }
 }
