41f6e4a461b6da77b6441731d680c0e0772906b8
Lua Zhi Zhan <zhizhanlua@gmail.com>
2019-12-14 23:37:32 +0800

    fix test

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,7 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { LoginPage } from './login.page';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -9,6 +14,7 @@ describe('LoginPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientModule, IonicModule],
       declarations: [LoginPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
